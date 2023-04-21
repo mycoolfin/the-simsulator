4fe81eafc0740b6d3546ec934bb060960a89d602
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-04-21 14:27:08 +0800

    Changed limb node instancing to breadth-first

--- Assets/Scripts/Genotype/Body/InstancedLimbNode.cs
+++ Assets/Scripts/Genotype/Body/InstancedLimbNode.cs
@@ -3,6 +3,7 @@ using System.Collections.ObjectModel;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class InstancedLimbNode : ILimbNodeEssentialInfo
 {
     [SerializeField] private string instanceId;
@@ -47,59 +48,60 @@ public class InstancedLimbNode : ILimbNodeEssentialInfo
             reflectedY ^ reflectY,
             reflectedZ ^ reflectZ
         );
-
         childLimbNodeInstances.Add(childInstance);
-
         return childInstance;
     }
 
-    private static List<InstancedLimbNode> RecursivelyDeriveInstancedLimbNodes(List<InstancedLimbNode> instanceList, IList<LimbNode> limbNodes,
-        InstancedLimbNode parentNodeInstance, LimbConnection connectionToParent, int nodeRecursionCount)
+    public static List<InstancedLimbNode> DeriveInstancedLimbNodes(IList<LimbNode> limbNodes)
     {
-        if (instanceList == null)
-            instanceList = new();
+        List<InstancedLimbNode> instanceList = new();
 
-        int nodeId = connectionToParent == null ? 0 : connectionToParent.ChildNodeId;
-        LimbNode node = limbNodes[nodeId];
+        Queue<(InstancedLimbNode, LimbConnection, int)> nodeQueue = new();
+        nodeQueue.Enqueue((null, null, 0));
 
-        List<InstancedLimbNode> newInstances = new();
-        if (connectionToParent == null)
+        while (nodeQueue.Count > 0)
         {
-            parentNodeInstance = new InstancedLimbNode("0", node, null, false, false, false);
-            newInstances.Add(parentNodeInstance);
-        }
-        else
-        {
-            int numberOfLimbs = instanceList.Count;
+            (InstancedLimbNode, LimbConnection, int) item = nodeQueue.Dequeue();
+            InstancedLimbNode parentNodeInstance = item.Item1;
+            LimbConnection connectionToParent = item.Item2;
+            int nodeRecursionCount = item.Item3;
 
-            bool canBeginReflectedSubtrees = nodeRecursionCount == 0 || (nodeRecursionCount == 1 && numberOfLimbs == 1);
-            bool beginReflectedX = canBeginReflectedSubtrees && connectionToParent.ReflectionX;
-            bool beginReflectedY = canBeginReflectedSubtrees && connectionToParent.ReflectionY;
-            bool beginReflectedZ = canBeginReflectedSubtrees && connectionToParent.ReflectionZ;
+            int nodeId = connectionToParent == null ? 0 : connectionToParent.ChildNodeId;
+            LimbNode node = limbNodes[nodeId];
 
-            int limbsToAdd = (int)Mathf.Pow(2, (beginReflectedX ? 1 : 0) + (beginReflectedY ? 1 : 0) + (beginReflectedZ ? 1 : 0));
-            bool canAddMoreLimbs = (numberOfLimbs + limbsToAdd) <= PhenotypeBuilderParameters.MaxLimbs;
-
-            bool recursing = node == parentNodeInstance.limbNode;
-            bool recursionLimitReached = nodeRecursionCount >= node.RecursiveLimit;
-            bool connectionCriteriaMet = (!recursionLimitReached && !connectionToParent.TerminalOnly) || (recursionLimitReached && connectionToParent.TerminalOnly) || (recursionLimitReached && !recursing);
-
-            if (!canAddMoreLimbs)
-                return null;
-
-            if (connectionCriteriaMet)
+            List<InstancedLimbNode> newInstances = new();
+            if (parentNodeInstance == null)
             {
-                newInstances.Add(parentNodeInstance.CreateChild(node, connectionToParent, false, false, false));
+                parentNodeInstance = new InstancedLimbNode("0", node, null, false, false, false);
+                newInstances.Add(parentNodeInstance);
+            }
+            else
+            {
+                int numberOfLimbs = instanceList.Count;
 
-                if (canBeginReflectedSubtrees)
+                bool canBeginReflectedSubtrees = nodeRecursionCount == 0 || (nodeRecursionCount == 1 && numberOfLimbs == 1);
+                bool beginReflectedX = canBeginReflectedSubtrees && connectionToParent.ReflectionX;
+                bool beginReflectedY = canBeginReflectedSubtrees && connectionToParent.ReflectionY;
+                bool beginReflectedZ = canBeginReflectedSubtrees && connectionToParent.ReflectionZ;
+
+                int potentialInstances = (int)Math.Pow(2, (connectionToParent.ReflectionX ? 1 : 0) + (connectionToParent.ReflectionY ? 1 : 0) + (connectionToParent.ReflectionZ ? 1 : 0));
+                int limbsToAdd = canBeginReflectedSubtrees ? potentialInstances : 1;
+                bool canAddMoreLimbs = (numberOfLimbs + limbsToAdd) <= PhenotypeBuilderParameters.MaxLimbs;
+
+                bool recursing = node == parentNodeInstance.limbNode;
+                bool recursionLimitReached = nodeRecursionCount >= node.RecursiveLimit;
+                bool connectionCriteriaMet = (!recursionLimitReached && !connectionToParent.TerminalOnly) || (recursionLimitReached && connectionToParent.TerminalOnly) || (recursionLimitReached && !recursing);
+
+                if (canAddMoreLimbs && connectionCriteriaMet)
                 {
+                    newInstances.Add(parentNodeInstance.CreateChild(node, connectionToParent, false, false, false));
+
                     List<bool> reflections = new List<bool> { beginReflectedX, beginReflectedY, beginReflectedZ };
                     for (int reflectIndex = 0; reflectIndex < reflections.Count; reflectIndex++)
                     {
                         if (reflections[reflectIndex])
                         {
                             int newLimbCount = newInstances.Count;
-
                             for (int limbIndex = 0; limbIndex < newLimbCount; limbIndex++)
                             {
                                 bool reflectX = reflectIndex == 0 || limbIndex % 2 != 0;
@@ -117,38 +119,27 @@ public class InstancedLimbNode : ILimbNodeEssentialInfo
                         }
                     }
                 }
+
+                int missingInstances = 0;
+                if (!(canAddMoreLimbs && connectionCriteriaMet))
+                    missingInstances = potentialInstances;
+                else if (!canBeginReflectedSubtrees)
+                    missingInstances = potentialInstances - 1;
+
+                for (int i = 0; i < missingInstances; i++)
+                    parentNodeInstance.childLimbNodeInstances.Add(null); // Add null children to preserve ordering.
             }
 
-            int missingChildren = 0;
-
-            if (!connectionCriteriaMet)
-                missingChildren += 1;
-
-            if (!connectionCriteriaMet || !canBeginReflectedSubtrees)
-            {
-                int unusedReflections = (int)Math.Pow(2,
-                    (connectionToParent.ReflectionX ? 1 : 0) + (connectionToParent.ReflectionY ? 1 : 0) + (connectionToParent.ReflectionZ ? 1 : 0))
-                    - 1;
-                missingChildren += unusedReflections;
-            }
-
-            for (int i = 0; i < missingChildren; i++)
-                parentNodeInstance.childLimbNodeInstances.Add(null); // Add null children to preserve child ordering.
-        }
-        newInstances.ForEach(l => instanceList.Add(l));
-
-        foreach (LimbConnection connection in node.Connections)
-        {
-            bool nextNodeIsThisNode = nodeId == connection.ChildNodeId;
             foreach (InstancedLimbNode newInstance in newInstances)
-                RecursivelyDeriveInstancedLimbNodes(instanceList, limbNodes, newInstance, connection, nodeRecursionCount + (nextNodeIsThisNode ? 1 : 0));
+            {
+                instanceList.Add(newInstance);
+                foreach (LimbConnection connection in node.Connections)
+                {
+                    bool nextNodeIsThisNode = nodeId == connection.ChildNodeId;
+                    nodeQueue.Enqueue((newInstance, connection, (nextNodeIsThisNode ? 1 : 0)));
+                }
+            }
         }
-
         return instanceList;
-    }
-
-    public static List<InstancedLimbNode> DeriveInstancedLimbNodes(IList<LimbNode> limbNodes)
-    {
-        return RecursivelyDeriveInstancedLimbNodes(null, limbNodes, null, null, 0);
     }
 }
