9ab002ea8e2db0d18241f4a91a38b4d7a5bb715b
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2023-05-03 13:08:44 +0800

    Added notarise script for Mac builds

---
+++ Build/Mac/sign_and_notarise.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source config
+
+echo "Signing and compressing..."
+chmod -R a+xr "$APP"
+codesign --deep --force --verify --verbose --timestamp --options runtime --entitlements "$ENTITLEMENTS" --sign "$CERTIFICATE" "$APP"
+ditto -c -k --sequesterRsrc --keepParent "$APP" "${APP}.zip"
+
+echo "Uploading compressed app to Apple for notarisation..."
+TEAM_ID=$(xcrun iTMSTransporter -m provider -u "$APPLE_ID" -p "$APPLE_APP_SPECIFIC_PASSWORD" | awk 'END{print $NF}')
+xcrun notarytool submit --wait --apple-id "$APPLE_ID" --password "$APPLE_APP_SPECIFIC_PASSWORD" --team-id="$TEAM_ID" "${APP}.zip"
+spctl -a -v "$APP"
