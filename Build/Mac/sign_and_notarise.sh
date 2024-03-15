f54c97456436bb334fe09a38feb07cc513e8fef8
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2024-03-15 10:18:59 +0800

    Mac build script fix

--- Build/Mac/sign_and_notarise.sh
+++ Build/Mac/sign_and_notarise.sh
@@ -8,6 +8,5 @@ codesign --deep --force --verify --verbose --timestamp --options runtime --entit
 ditto -c -k --sequesterRsrc --keepParent "$APP" "$APP.zip"
 
 echo "Uploading compressed app to Apple for notarisation..."
-TEAM_ID=$(xcrun iTMSTransporter -m provider -u "$APPLE_ID" -p "$APPLE_APP_SPECIFIC_PASSWORD" | awk 'END{print $NF}')
 xcrun notarytool submit --wait --apple-id $APPLE_ID --password $APPLE_APP_SPECIFIC_PASSWORD --team-id=$TEAM_ID "$APP".zip
 spctl -a -v "$APP"
