54b165251a1f98ac3e96a7aedd17a7ef10260325
mycoolfin <32662382+mycoolfin@users.noreply.github.com>
2025-09-07 16:29:42 +0800

    Web link button cursor unlock fix

--- Assets/Scripts/Core/UI/3D/WebLinkButton.cs
+++ Assets/Scripts/Core/UI/3D/WebLinkButton.cs
@@ -2,17 +2,23 @@ using UnityEngine;
 
 namespace mycoolfin.TheSimsulator.UnityIntegration.Core.UI.ThreeD
 {
+    using Player.FPS;
+
     public class WebLinkButton : MonoBehaviour
     {
         [SerializeField] private PushButton button;
         [SerializeField] private string url = string.Empty;
+        [SerializeField] private FPSPlayerController playerController;
 
         private void Start()
         {
             button.OnButtonPressed += (_) =>
             {
                 if (!string.IsNullOrEmpty(url))
+                {
                     Application.OpenURL(url);
+                    playerController.SetCursorLock(false);
+                }
             };
         }
     }
